4af92cedb718aa9367cf247db6262934ea7f83c5
cionita <costin.ionita@mountaindayz.com>
2014-01-17 15:41:09 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodOracle::Application.config.secret_token = 'bd85d2d00e3e2d57b40b1978330df4739b5ba1831db9d21e8c8a1b4c79ce673c4cbb1fc8eee3c3ce698e642f6c945df1d4b32a263d479df3637e8cc3068b30b8'
