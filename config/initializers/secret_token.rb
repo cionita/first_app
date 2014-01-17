948d0987f29beae1e4a01b998b686d2647f75926
cionita <costin.ionita@mountaindayz.com>
2014-01-17 17:04:41 +0100

    finished creating basic resources

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,27 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Twitter::Application.config.secret_token = '812694d69a1799ddb32915eaf13f2a1c19702b928dd547e9f38a2429bee5f7b2d67dde5e7e555b4438a32f975c17b21651431da6dc4d0d61475f67496d02abf1'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+FoodOracle::Application.config.secret_key_base = secure_token
