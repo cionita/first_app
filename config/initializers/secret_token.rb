c9501ca967142e7c61117b97d0a4d086c367ab49
cionita <costin.ionita@mountaindayz.com>
2014-01-17 17:00:55 +0100

    finished creating basic resources

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FoodOracle::Application.config.secret_token = 'bd85d2d00e3e2d57b40b1978330df4739b5ba1831db9d21e8c8a1b4c79ce673c4cbb1fc8eee3c3ce698e642f6c945df1d4b32a263d479df3637e8cc3068b30b8'
+Twitter::Application.config.secret_token = '812694d69a1799ddb32915eaf13f2a1c19702b928dd547e9f38a2429bee5f7b2d67dde5e7e555b4438a32f975c17b21651431da6dc4d0d61475f67496d02abf1'
