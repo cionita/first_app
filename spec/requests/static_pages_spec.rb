35b70f3bf4fdc444b11772b584571459f01a3d93
cionita <costin.ionita@mountaindayz.com>
2014-01-20 18:00:25 +0100

    finished static pages, with failed tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,8 @@ describe "Static pages" do
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Home')
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -24,7 +25,8 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text => 'Help')
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -37,7 +39,8 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', :text => 'About Us')
+      page.should have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
