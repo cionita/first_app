2349c52b2468610dadbb4be9679ef59e2d02c6f8
cionita <costin.ionita@mountaindayz.com>
2014-02-03 15:38:01 +0100

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140131112159) do
+ActiveRecord::Schema.define(:version => 20140203140806) do
 
   create_table "menu_items", :force => true do |t|
     t.string   "name"
@@ -36,10 +36,11 @@ ActiveRecord::Schema.define(:version => 20140131112159) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
